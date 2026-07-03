a213dda045070c1504da8f5125fa6e7a6fabd0fe
WhiteSnake <whitesnake35711@gmail.com>
2026-07-03 23:15:32 +0530

    minor bug fixes

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -45,7 +45,7 @@ const generateOAuthToken = (
   if (!process.env.IDP_PRIVATE_KEY) throw new Error("IDP_PRIVATE_KEY not set");
 
   const privateKey = createPrivateKey({
-    key: Buffer.from(process.env.IDP_PRIVATE_KEY_B64!, "base64").toString("utf8"),
+    key: Buffer.from(process.env.IDP_PRIVATE_KEY!, "base64").toString("utf8"),
     format: "pem",
   });
 
