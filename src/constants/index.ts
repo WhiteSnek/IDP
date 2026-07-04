54f056f8a71bfd2b917d51182bbb450c60c1e159
WhiteSnake <whitesnake35711@gmail.com>
2026-07-04 13:16:02 +0530

    fixed cookie options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,10 +2,11 @@ import { CookieOptions } from "express";
 
 export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
-  sameSite: "lax",
-  secure: false,
+  secure: true,
+  sameSite: "none",
+  path: "/",
 };
 
-export const ALL_CHANNELS = ["email", "sms", "whatsapp", "push"]
+export const ALL_CHANNELS = ["email", "sms", "whatsapp", "push"];
 
-export const NOTIFICATION_PERMISSION = true
+export const NOTIFICATION_PERMISSION = true;
