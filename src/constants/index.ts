2b27a5133be1efa10bd8d2d1f99f6baec8e1be0f
WhiteSnake <whitesnake35711@gmail.com>
2026-07-05 14:33:36 +0530

    added domain in the cookies

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,16 +1,20 @@
 import { CookieOptions } from "express";
 
-export const COOKIE_OPTIONS: CookieOptions = process.env.NODE_ENV === "production" ? {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  path: "/",
-} : {
-  httpOnly: true,
-  secure: false,
-  sameSite: "lax",
-  path: "/",
-};
+export const COOKIE_OPTIONS: CookieOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        domain: ".whitesnek.xyz",
+        path: "/",
+      }
+    : {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        path: "/",
+      };
 
 export const ALL_CHANNELS = ["email", "sms", "whatsapp", "push"];
 
