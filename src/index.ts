a1e2841356c38bcadd29e21a6391c3919537d44e
WhiteSnake <whitesnake35711@gmail.com>
2026-07-05 14:45:16 +0530

    removed cors

--- src/index.ts
+++ src/index.ts
@@ -15,27 +15,6 @@ dotenv.config({
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
-const allowedOrigins = [
-  process.env.FRONTEND_URL,          
-  "http://localhost:5173",           
-  "http://127.0.0.1:5173",
-  "http://192.168.1.200:5173",       
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
 
 app.set("trust proxy", 1);
 
